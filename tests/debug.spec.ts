495f48b3cd8d19889c7864a6d61484684b1aa8ce
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-10-09 20:01:37 +0700

    remove debug

--- tests/debug.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "@playwright/test";
-import { goToLiveAttendance } from "./utils";
-
-test("Debug", async ({ page }) => {
-  await goToLiveAttendance(page);
-
-  await expect(page.getByText("Attendance log")).toBeVisible();
-});
