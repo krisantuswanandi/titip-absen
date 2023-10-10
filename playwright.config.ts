3bdd32179e300873a26d7e6a346001c9309cc4d3
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-10-10 09:47:31 +0700

    artifact

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   forbidOnly: true,
   retries: 0,
   workers: 1,
-  use: { trace: "on-first-retry" },
+  use: { video: "on" },
   projects: [
     {
       name: "chromium",
